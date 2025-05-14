43a7665c1ec2b12229f6d40e07f7a0497d831d28
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-05-14 14:54:20 +0100

    Type signatures

--- src/List.hs
+++ src/List.hs
@@ -1,8 +1,64 @@
 module List where
 
 data IntList = Empty | Node Int IntList
-  deriving (Show)
+  deriving (Show, Eq)
 
+data MaybeInt = NoInt | JustInt Int
+  deriving (Show, Eq)
+
+twoNumbers :: IntList
 twoNumbers = Node 1 (Node 2 Empty)
 
-intHead =
+intHead :: IntList -> MaybeInt
+intHead list =
+	case list of
+		Node n xs -> JustInt n
+		Empty -> NoInt
+
+add1 :: Int -> Int
+add1 a = a + 1
+
+tripple :: Int -> Int
+tripple a = a * 3
+
+f :: Int -> (Int -> Int)
+f x =
+  if x > 10
+  then add1
+  else tripple
+
+lengthIntList :: IntList -> Int
+lengthIntList list =
+	case list of
+		Node a b -> (1 + lengthIntList b)
+		Empty -> 0
+
+
+-- Get the last element of the list
+--
+--    lastIntList (Node 1 (Node 2 Empty)) == JustInt 2
+--
+lastIntList :: IntList -> MaybeInt
+lastIntList = undefined
+
+-- Apply function f to every element of the list
+--
+--     mapIntList tripple (Node 1 (Node 2 Empty)) == Node 3 (Node 6 Empty)
+--
+mapIntList :: (Int -> Int) -> IntList -> IntList
+mapIntList f xs = undefined
+
+-- Merge two IntLists
+--
+--  concatIntList (Node 1 (Node 2 Empty)) (Node 3 (Node 4 Empty)) ==
+--      Node 1 (Node 2 (Node 3 (Node 4 Empty)))
+--
+	concatIntList :: IntList -> IntList -> IntList
+concatIntList xs ys = undefined
+
+-- Return the reversed list
+--
+--  reverseIntList (Node 1 (Node 2 Empty)) == Node 2 (Node 1 Empty)
+--
+reverseIntList :: IntList -> IntList
+reverseIntList xs = undefined
--- src/Other.hs
+++ src/Other.hs
@@ -1,3 +1,16 @@
 module Other where
 
+myVar :: Int
 myVar = 2
+
+add1 :: Int -> Int
+add1 a = a + 1
+
+tripple :: Int -> Int
+tripple a = a * 3
+
+f x =
+  if x > 10
+  then add1
+  else tripple
+
