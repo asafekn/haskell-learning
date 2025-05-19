6881d448f2c07054063cb05a23864f732907c16b
Marcelo Lazaroni <lazaronijunior@gmail.com>
2025-05-19 11:15:24 +0100

    Add infix operator for IntList

--- src/List.hs
+++ src/List.hs
@@ -1,21 +1,26 @@
 module List where
 
-data IntList = Empty | Node Int IntList
+import Prelude hiding ((<>), (:))
+
+data IntList = Empty | (:::) Int IntList
   deriving (Show, Eq)
+
+infixr 8 :::
+infixr 2 <>
 
 data MaybeInt = NoInt | JustInt Int
   deriving (Show, Eq)
 
 twoNumbers :: IntList
-twoNumbers = Node 1 (Node 2 Empty)
+twoNumbers = 1 ::: 2 ::: Empty
 
 anotherTwoNumbers :: IntList
-anotherTwoNumbers = Node 3 (Node 4 Empty)
+anotherTwoNumbers = 3 ::: (4 ::: Empty)
 
 intHead :: IntList -> MaybeInt
 intHead list =
 	case list of
-		Node n xs -> JustInt n
+		n ::: xs -> JustInt n
 		Empty -> NoInt
 
 add1 :: Int -> Int
@@ -33,49 +38,52 @@ f x =
 lengthIntList :: IntList -> Int
 lengthIntList list =
 	case list of
-		Node a b -> (1 + lengthIntList b)
+		a ::: b -> (1 + lengthIntList b)
 		Empty -> 0
 
 -- Get the last element of the list
 --
---    lastIntList (Node 1 (Node 2 Empty)) == JustInt 2
+--    lastIntList (1 ::: (2 ::: Empty)) == JustInt 2
 --
 lastIntList :: IntList -> MaybeInt
 lastIntList list =
 	case list of
-		Node a b -> case b of
-			Node c d -> lastIntList b
+		a ::: b -> case b of
+			c ::: d -> lastIntList b
 			Empty -> JustInt a
 
 		Empty -> NoInt
 
 -- Apply function f to every element of the list
 --
---     mapIntList tripple (Node 1 (Node 2 Empty)) == Node 3 (Node 6 Empty)
+--     mapIntList tripple (1 ::: (2 ::: Empty)) == 3 ::: (6 ::: Empty)
 --
 mapIntList :: (Int -> Int) -> IntList -> IntList
 mapIntList f xs =
 	case xs of
-		Node a b -> Node(f a) (mapIntList f b)
+		a ::: b -> f a ::: mapIntList f b
 		Empty -> Empty
 
 -- Merge two IntLists
 --
---  concatIntList (Node 1 (Node 2 Empty)) (Node 3 (Node 4 Empty)) ==
---      Node 1 (Node 2 (Node 3 (Node 4 Empty)))
+--  concatIntList (1 ::: (2 ::: Empty)) (3 ::: (4 ::: Empty)) ==
+--      1 ::: (2 ::: (3 ::: (4 ::: Empty)))
 --
 concatIntList :: IntList -> IntList -> IntList
 concatIntList xs ys =
 	case xs of
-		Node a b -> Node a (concatIntList b ys)
+		a ::: b -> a ::: (concatIntList b ys)
 		Empty -> ys
+
+(<>) :: IntList -> IntList -> IntList
+(<>) = concatIntList
 
 -- Return the reversed list
 --
---  reverseIntList (Node 1 (Node 2 Empty)) == Node 2 (Node 1 Empty)
+--  reverseIntList (1 ::: (2 ::: Empty)) == 2 ::: (1 ::: Empty)
 --
 reverseIntList :: IntList -> IntList
 reverseIntList xs =
 	case xs of
-		Node a b -> concatIntList (reverseIntList b) (Node a Empty)
+		a ::: b -> concatIntList (reverseIntList b) (a ::: Empty)
 		Empty -> Empty
